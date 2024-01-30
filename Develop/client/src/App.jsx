69523d532bbe66e4e72f20eb4aa5945c091432fd
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-01-30 15:03:08 -0500

    setcontext

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink,} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Navbar from './components/Navbar';
 
 // Construct our main GraphQL API endpoint
